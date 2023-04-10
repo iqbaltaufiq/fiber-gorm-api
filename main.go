2952ea13dcccc7db81dc40d794124f6ae0f3542a
Iqbal Taufiq <iqbaltaufiq@outlook.com>
2023-04-10 21:28:28 +0700

    implementing CORS.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/iqbaltaufiq/go-fiber-restapi/controllers/bookcontroller"
 	"github.com/iqbaltaufiq/go-fiber-restapi/models"
 )
@@ -12,6 +13,12 @@ func main() {
 	models.NewDBConnection()
 
 	app := fiber.New()
+
+	// implement CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://127.0.0.1:5500",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	api := app.Group("/api")
 	api.Get("/books", bookcontroller.FindAll)
